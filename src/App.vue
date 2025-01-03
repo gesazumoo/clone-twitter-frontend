1634a5c2daee0fe86bcd1d41bcc2b0907d431959
gesazumoo <gesazumoo@huinnoaim.com>
2025-01-03 14:54:30 +0900

    template syntax1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div>{{ msg }}</div>
+    <button :disabled="isButtonDisabled">button</button>
+    {{ 1 + 1 }}
+    {{ "msg : " + msg }}
+    {{ true ? "true" : "not" }}
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      msg: "Hello, Vue!",
+      isButtonDisabled: false,
+    };
+  },
 };
 </script>
 
